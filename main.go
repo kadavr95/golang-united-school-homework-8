42acd5cc749d40db5c24b52838837cbc33fc8945
Dmitry Yaskovich <kadavr95@outlook.com>
2022-06-17 23:54:41 +0300

    Improve code readability

--- main.go
+++ main.go
@@ -22,138 +22,63 @@ var (
 	operationFlag = flag.String("operation", "", "")
 	itemFlag      = flag.String("item", "", "")
 	filenameFlag  = flag.String("filename", "", "")
+
+	validOperations = map[string]bool{
+		"add":      true,
+		"list":     true,
+		"findById": true,
+		"remove":   true,
+	}
 )
 
 func Perform(args Arguments, writer io.Writer) error {
-	switch args["operation"] {
-	case "":
+	if args["operation"] == "" {
 		return fmt.Errorf("-operation flag has to be specified")
-	case
-		"add",
-		"list",
-		"findById",
-		"remove":
-		if args["fileName"] == "" {
-			return fmt.Errorf("-fileName flag has to be specified")
-		}
-		file, err := os.OpenFile(args["fileName"], os.O_RDWR|os.O_CREATE, 644)
-		defer func(file *os.File) {
-			err := file.Close()
-			if err != nil {
-
-			}
-		}(file)
-		if err != nil {
-			return fmt.Errorf("file can't be opened")
-		}
-		fileContents, err := ioutil.ReadAll(file)
-		if err != nil {
-			return fmt.Errorf("file can't be read")
-		}
-		switch args["operation"] {
-		case "add":
-			if args["item"] == "" {
-				return fmt.Errorf("-item flag has to be specified")
-			}
-			var users []User
-			if len(fileContents) > 0 {
-				err := json.Unmarshal(fileContents, &users)
-				if err != nil {
-					return fmt.Errorf("json parse error")
-				}
-			}
-			var newUser User
-			err = json.Unmarshal([]byte(args["item"]), &newUser)
-			if err != nil {
-				return fmt.Errorf("new item json parse error")
-			}
-			notDuplicateFlag := true
-			for _, val := range users {
-				if val.Id == newUser.Id {
-					_, err = writer.Write([]byte(fmt.Sprintf("Item with id %s already exists", newUser.Id)))
-					notDuplicateFlag = false
-					if err != nil {
-						return fmt.Errorf("error to output Existing id message")
-					}
-				}
-			}
-			if notDuplicateFlag {
-				users = append(users, newUser)
-				fileData, err := json.Marshal(users)
-				if err != nil {
-					return fmt.Errorf("error converting structure to json")
-				}
-				err = os.WriteFile(args["fileName"], fileData, 644)
-				if err != nil {
-					return fmt.Errorf("error writing to file")
-				}
-			}
-		case "list":
-			_, err := writer.Write(fileContents)
-			if err != nil {
-				return fmt.Errorf("file contents can't be returned")
-			}
-		case "findById":
-			if args["id"] == "" {
-				return fmt.Errorf("-id flag has to be specified")
-			}
-			var users []User
-			if len(fileContents) > 0 {
-				err := json.Unmarshal(fileContents, &users)
-				if err != nil {
-					return fmt.Errorf("json parse error")
-				}
-			}
-			for _, val := range users {
-				if val.Id == args["id"] {
-					result, err := json.Marshal(val)
-					if err != nil {
-						return fmt.Errorf("error converting result to json")
-					}
-					_, err = writer.Write(result)
-					if err != nil {
-						return fmt.Errorf("error to output Existing id message")
-					}
-				}
-			}
-		case "remove":
-			if args["id"] == "" {
-				return fmt.Errorf("-id flag has to be specified")
-			}
-			var users []User
-			if len(fileContents) > 0 {
-				err := json.Unmarshal(fileContents, &users)
-				if err != nil {
-					return fmt.Errorf("json parse error")
-				}
-			}
-			notFoundFlag := true
-			for i, val := range users {
-				if val.Id == args["id"] {
-					notFoundFlag = false
-					users[i] = users[len(users)-1]
-					users = users[:len(users)-1]
-					fileData, err := json.Marshal(users)
-					if err != nil {
-						return fmt.Errorf("error converting structure to json")
-					}
-					err = os.WriteFile(args["fileName"], fileData, 644)
-					if err != nil {
-						return fmt.Errorf("error writing to file")
-					}
-				}
-			}
-			if notFoundFlag {
-				_, err = writer.Write([]byte(fmt.Sprintf("Item with id %s not found", args["id"])))
-				if err != nil {
-					return fmt.Errorf("error to output Existing id message")
-				}
-			}
-		}
-		return nil
-	default:
+	}
+	if !validOperations[args["operation"]] {
 		return fmt.Errorf("Operation %s not allowed!", args["operation"])
 	}
+
+	if args["fileName"] == "" {
+		return fmt.Errorf("-fileName flag has to be specified")
+	}
+
+	file, err := os.OpenFile(args["fileName"], os.O_RDWR|os.O_CREATE, 644)
+	defer file.Close()
+
+	if err != nil {
+		return fmt.Errorf("file can't be opened")
+	}
+
+	fileContents, err := ioutil.ReadAll(file)
+	if err != nil {
+		return fmt.Errorf("file can't be read")
+	}
+
+	switch args["operation"] {
+	case "add":
+		err = add(fileContents, writer, args)
+		if err != nil {
+			return err
+		}
+	case "list":
+		err = list(fileContents, writer)
+		if err != nil {
+			return err
+		}
+	case "findById":
+		err = findById(fileContents, writer, args)
+		if err != nil {
+			return err
+		}
+	case "remove":
+		err = remove(fileContents, writer, args)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
 
 func main() {
@@ -171,4 +96,139 @@ func parseArgs() Arguments {
 		"item":      *itemFlag,
 		"fileName":  *filenameFlag,
 	}
+}
+
+func add(fileContents []byte, writer io.Writer, args Arguments) error {
+	if args["item"] == "" {
+		return fmt.Errorf("-item flag has to be specified")
+	}
+
+	users, err := parseUsers(fileContents)
+	if err != nil {
+		return err
+	}
+
+	var newUser User
+	err = json.Unmarshal([]byte(args["item"]), &newUser)
+	if err != nil {
+		return fmt.Errorf("new item json parse error")
+	}
+
+	notDuplicateFlag := true
+
+	for _, val := range users {
+		if val.Id == newUser.Id {
+			_, err = writer.Write([]byte(fmt.Sprintf("Item with id %s already exists", newUser.Id)))
+			if err != nil {
+				return fmt.Errorf("error to output Existing id message")
+			}
+
+			notDuplicateFlag = false
+		}
+	}
+
+	if notDuplicateFlag {
+		users = append(users, newUser)
+
+		fileData, err := json.Marshal(users)
+		if err != nil {
+			return fmt.Errorf("error converting structure to json")
+		}
+
+		err = os.WriteFile(args["fileName"], fileData, 644)
+		if err != nil {
+			return fmt.Errorf("error writing to file")
+		}
+	}
+
+	return nil
+}
+
+func findById(fileContents []byte, writer io.Writer, args Arguments) error {
+	if args["id"] == "" {
+		return fmt.Errorf("-id flag has to be specified")
+	}
+
+	users, err := parseUsers(fileContents)
+	if err != nil {
+		return err
+	}
+
+	for _, val := range users {
+		if val.Id == args["id"] {
+			result, err := json.Marshal(val)
+			if err != nil {
+				return fmt.Errorf("error converting result to json")
+			}
+
+			_, err = writer.Write(result)
+			if err != nil {
+				return fmt.Errorf("error to output Existing id message")
+			}
+		}
+	}
+
+	return nil
+}
+
+func list(fileContents []byte, writer io.Writer) error {
+	_, err := writer.Write(fileContents)
+	if err != nil {
+		return fmt.Errorf("file contents can't be returned")
+	}
+
+	return nil
+}
+
+func remove(fileContents []byte, writer io.Writer, args Arguments) error {
+	if args["id"] == "" {
+		return fmt.Errorf("-id flag has to be specified")
+	}
+
+	users, err := parseUsers(fileContents)
+	if err != nil {
+		return err
+	}
+
+	notFoundFlag := true
+
+	for i, val := range users {
+		if val.Id == args["id"] {
+			users[i] = users[len(users)-1]
+			users = users[:len(users)-1]
+
+			fileData, err := json.Marshal(users)
+			if err != nil {
+				return fmt.Errorf("error converting structure to json")
+			}
+
+			err = os.WriteFile(args["fileName"], fileData, 644)
+			if err != nil {
+				return fmt.Errorf("error writing to file")
+			}
+
+			notFoundFlag = false
+		}
+	}
+
+	if notFoundFlag {
+		_, err := writer.Write([]byte(fmt.Sprintf("Item with id %s not found", args["id"])))
+		if err != nil {
+			return fmt.Errorf("error to output Existing id message")
+		}
+	}
+	return nil
+}
+
+func parseUsers(fileContents []byte) ([]User, error) {
+	var users []User
+
+	if len(fileContents) > 0 {
+		err := json.Unmarshal(fileContents, &users)
+		if err != nil {
+			return nil, fmt.Errorf("json parse error")
+		}
+	}
+
+	return users, nil
 }
